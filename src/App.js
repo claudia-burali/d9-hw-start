bd656e4d2d4fe2033b0e6df0389a493434b45747
claudia-burali <161461840+claudia-burali@users.noreply.github.com>
2024-05-06 17:07:04 +0200

    mod

--- src/App.js
+++ src/App.js
@@ -2,25 +2,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MainSearch from "./components/MainSearch";
 import CompanySearchResults from "./components/CompanySearchResults";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = () => {
-  const companySelected = useSelector((state) => state.companySelected.content);
+function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Link to="/">
-          <h1>EpicJobs</h1>
-          {companySelected}
-        </Link>
-      </div>
       <Routes>
         <Route path="/" element={<MainSearch />} />
         <Route path="/:company" element={<CompanySearchResults />} />
       </Routes>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -3,14 +3,17 @@ import { Link } from 'react-router-dom';
 import { useDispatch } from "react-redux";
 
 const Job = ({ data }) => {
-  
+  const dispatch = useDispatch()
   return (
   <Row
     className="mx-0 mt-3 p-3"
     style={{ border: '1px solid #00000033', borderRadius: 4 }}
   >
     <Col xs={3}>
-      <Button>+</Button>
+      <Button onClick={() => {dispatch({
+              type: "ADD_TO_FAVORITES",
+              payload: data.company_name,
+            });}}>+</Button>
       <Link to={`/${data.company_name}`}>{data.company_name}</Link>
     </Col>
     <Col xs={9}>
